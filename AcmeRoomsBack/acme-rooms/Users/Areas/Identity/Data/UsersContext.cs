9095dca541221733f4f5d973d4bc8cdfd61d9dfc
jolichh <113618956+jolichh@users.noreply.github.com>
2023-10-23 13:57:30 +0200

    microservicios

---
+++ AcmeRoomsBack/acme-rooms/Users/Areas/Identity/Data/UsersContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Users.Areas.Identity.Data;
+
+namespace Users.Data;
+
+public class UsersContext : IdentityDbContext<UsersUser>
+{
+    public UsersContext(DbContextOptions<UsersContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
