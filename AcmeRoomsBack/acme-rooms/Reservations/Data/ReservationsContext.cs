f3a502ad94b248ff935739a03aa1df34376461a8
john.uy.ext <john.uy.ext@inetum.com>
2023-10-25 09:30:26 +0200

    Microservicio Reservations

---
+++ AcmeRoomsBack/acme-rooms/Reservations/Data/ReservationsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Reservation.Entities;
+
+namespace Reservations.Data
+{
+    public class ReservationsContext : DbContext
+    {
+        public ReservationsContext (DbContextOptions<ReservationsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Reservation.Entities.Reservation> Reservation { get; set; } = default!;
+    }
+}
--- AcmeRoomsBack/acme-rooms/Reservations/Program.cs
+++ AcmeRoomsBack/acme-rooms/Reservations/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Reservations.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ReservationsContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ReservationsContext") ?? throw new InvalidOperationException("Connection string 'ReservationsContext' not found.")));
 
 // Add services to the container.
 
