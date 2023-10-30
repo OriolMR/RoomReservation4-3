f1eeb7a413084a6ea4b3b9c703564fe756d744b5
GuillermoJM-Inetum <guillermo.jaume@inetum.com>
2023-10-30 13:50:51 +0100

    UPDATE botones Login and Register

---
+++ MainProject/acme-rooms/src/app/services/reserves-service/reserves.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { __rest } from 'tslib';
+/** 
+@Injectable({
+    providedIn: 'root',
+})
+*/
+export interface ReservesService {
+}
